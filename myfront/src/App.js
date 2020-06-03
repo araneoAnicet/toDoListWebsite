633293f4af76c6ad001a09537f45025f07861780
DeonOni <maksimtrush905@gmail.com>
2020-06-03 21:02:06 +0300

    add navbar

--- myfront/src/App.js
+++ myfront/src/App.js
@@ -1,16 +1,44 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-//import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap'
+import "bootswatch/dist/lux/bootstrap.min.css"
 
 function App() {
   return (
     <div>
-      <h1>Vova pidor</h1>
-      <Button variant="primary">Button</Button>
+
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor02">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search"></input>
+            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+
     </div>
   )
+  
 }
 
 export default App;
