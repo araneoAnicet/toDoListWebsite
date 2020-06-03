9074ff3fbd36eb15d343e2c661e2782d539b113e
araneoAnicet <araneoanicet@gmail.com>
2020-06-03 23:27:07 +0100

    fix relationships

--- app/src/models.py
+++ app/src/models.py
@@ -4,12 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 sql_db = SQLAlchemy()
 
 projects = sql_db.Table(
+    'projects',
     sql_db.Column('project_id', sql_db.Integer, sql_db.ForeignKey('project.id'), primary_key=True),
     sql_db.Column('user_id', sql_db.Integer, sql_db.ForeignKey('user.id'), primary_key=True)
 )
 
 
 class User(sql_db.Model):
+    __tablename__ = 'user'
     id = sql_db.Column(sql_db.Integer, primary_key=True)
     username = sql_db.Column(sql_db.String(50), unique=False, nullable=False)
     email = sql_db.Column(sql_db.String(90), unique=True, nullable=False)
@@ -23,17 +25,19 @@ class User(sql_db.Model):
 
 
 class Project(sql_db.Model):
+    __tablename__ = 'project'
     id = sql_db.Column(sql_db.Integer, primary_key=True)
     name = sql_db.Column(sql_db.String(90), unique=False, nullable=True)
     description = sql_db.Column(sql_db.String(360), unique=False, nullable=True)
-    owner_id = sql_db.Column(sql_db.Integer, sql_db.ForeignKey('owner.id'), nullable=False)
-    tasts = sql_db.relationship('Task', backref='project', lazy=True)
+    owner_id = sql_db.Column(sql_db.Integer, sql_db.ForeignKey('user.id'), nullable=False)
+    tasks = sql_db.relationship('Task', backref='project', lazy=True)
 
     def __repr__(self):
         return f'Project({self.name}::{self.owner_id})'
 
 
 class Task(sql_db.Model):
+    __tablename__ = 'task'
     id = sql_db.Column(sql_db.Integer, primary_key=True)
     name = sql_db.Column(sql_db.String(90), unique=True, nullable=False)
     description = sql_db.Column(sql_db.String(360), unique=False, nullable=True)
