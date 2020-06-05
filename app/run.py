6a7a3c939c3299290fb401ab9db3f79f833bbdc7
araneoAnicet <araneoanicet@gmail.com>
2020-06-05 18:46:40 +0100

    add user registration

--- app/run.py
+++ app/run.py
@@ -1,16 +1,21 @@
 from src.models import  sql_db
-from src.api.routes import api_blueprint
+from src.api.routes import api_blueprint, bcrypt
 from src.admin.routes import admin, admin_blueprint
+from flask_cors import CORS
 from flask import Flask
+
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'some_secret_key'
+CORS(app)
 
 with app.app_context():
     admin.init_app(app)
     sql_db.init_app(app)
+    bcrypt.init_app(app)
 
 app.register_blueprint(api_blueprint, url_prefix='/api')
 app.register_blueprint(admin_blueprint)
--- app/src/api/routes.py
+++ app/src/api/routes.py
@@ -1,15 +1,54 @@
 from flask_restful import Api, Resource
-from flask import Blueprint, jsonify
-from src.models import sql_db
+from flask import Blueprint, jsonify, request
+from src.models import sql_db, User
+from flask_bcrypt import Bcrypt
+from werkzeug.security import safe_str_cmp
 
 
 api_blueprint = Blueprint('api', __name__)
+bcrypt = Bcrypt()
 api = Api(api_blueprint)
 
+def json_response(status, message, data):
+    return {
+        'status': status,
+        'message': message,
+        'data': data,
+        'payload': {
+            'headers': dict(request.headers),
+            'url': request.url,
+            'method': request.method,
+        }
+    }
 
 class UserResource(Resource):
     def get(self):
         pass
+
+    def post(self):
+        # registration
+        user_data = {
+            'username': request.form.get('username'),
+            'email': request.form.get('email'),
+            'password': request.form.get('password'),
+            'repeat_password': request.form.get('repeat_password')
+        }
+        if not User.query.filter_by(email=user_data['email']).first():
+            if safe_str_cmp(user_data['password'], user_data['repeat_password']):
+                new_user = User(
+                    username=user_data['username'],
+                    email=user_data['email'],
+                    password=str(bcrypt.generate_password_hash(user_data['password']), 'utf-8')
+                )
+                sql_db.session.add(new_user)
+                sql_db.session.commit()
+                response_user_data = {
+                    'username': user_data['username'],
+                    'email': user_data['email']
+                }
+                return json_response(200, 'OK', {'user': response_user_data})
+            return json_response(401, 'Passwords should be equal', None)
+        return json_response(401, 'This user already exists!', None)
 
     def put(self):
         pass
@@ -17,11 +56,8 @@ class UserResource(Resource):
     def delete(self):
         pass
 
-    def post(self):
-        pass
 
-
-@api.route('/test')
+@api_blueprint.route('/test')
 def test():
     return jsonify({
         'message': 'Hello world!'
