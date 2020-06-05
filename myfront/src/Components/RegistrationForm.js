88669e664a80a7d8b322ae6775600c509819ad4d
DeonOni <maksimtrush905@gmail.com>
2020-06-05 22:12:53 +0300

    Add registration form

---
+++ myfront/src/Components/RegistrationForm.js
@@ -0,0 +1,37 @@
+import React,{ Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootswatch/dist/lux/bootstrap.min.css";
+
+class RegistrationForm extends Component {
+    render(){
+    return(
+        
+        <div>
+        <form className="LoginForm-margin">
+        <fieldset>
+
+            <legend>Legend</legend>
+            <div className="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div className="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Repeat password"></input>
+            </div>
+            
+            
+            <button typw="button" className="btn btn-primary">Sign in</button>
+            <button type="button" className="btn btn-secondary">Sign up</button>
+        </fieldset>
+        </form>
+        </div>
+        
+)
+    }
+}
+
+export default RegistrationForm;
