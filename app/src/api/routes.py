c638bf44c1439231e11ff29923d155b79d865bc6
araneoAnicet <araneoanicet@gmail.com>
2020-06-05 16:54:04 +0100

    add test route

--- app/src/api/routes.py
+++ app/src/api/routes.py
@@ -1,5 +1,5 @@
 from flask_restful import Api, Resource
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from src.models import sql_db
 
 
@@ -9,7 +9,7 @@ api = Api(api_blueprint)
 
 class UserResource(Resource):
     def get(self):
-        return {'message': 'Hello from REST API!'}
+        pass
 
     def put(self):
         pass
@@ -19,5 +19,12 @@ class UserResource(Resource):
 
     def post(self):
         pass
+
+
+@api.route('/test')
+def test():
+    return jsonify({
+        'message': 'Hello world!'
+    })
 
 api.add_resource(UserResource, '/user')
