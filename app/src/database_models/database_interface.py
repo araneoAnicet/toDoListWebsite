39ce09b458f103c1ee7da79b5b9b0727866664c7
araneoAnicet <araneoanicet@gmail.com>
2020-06-05 22:27:00 +0100

    add class set

--- app/src/database_models/database_interface.py
+++ app/src/database_models/database_interface.py
@@ -1,4 +1,7 @@
 class UserDatabaseInterface:
+    def set_user_class(self, user_class_ref):
+        self.user_class_ref = user_class_ref
+
     def user_registration(self, username: str, email: str, password: str) -> None:
         pass
 
@@ -7,6 +10,12 @@ class UserDatabaseInterface:
 
 
 class ProjectDatabaseInterface:
+    def set_project_class(self, project_class_ref):
+        self.project_class_ref = project_class_ref
+
+    def set_task_class(self, task_class_ref):
+        self.task_class_ref = task_class_ref
+
     def add_project(self, owner, name: str, description: str) -> None:
         pass
 
