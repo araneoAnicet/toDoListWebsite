1e7f8fb20a8b87ab494d0bc51822b083c776ad2d
DeonOni <maksimtrush905@gmail.com>
2020-06-06 11:58:25 +0300

    make code looks better

--- myfront/src/Components/LoginForm.js
+++ myfront/src/Components/LoginForm.js
@@ -12,22 +12,21 @@ class LoginForm extends Component {
         
         <div>
         <form className="LoginForm-margin">
-        <fieldset>
-
-            <legend>Log in</legend>
-            <div className="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
-            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-            </div>
-            <div className="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"></input>
-            </div>
-            
-            <button typw="button" className="btn btn-primary">Sign in</button>
-            <button type="button" className="btn btn-secondary">Sign up</button>
-        </fieldset>
+            <fieldset>
+                <legend>Log in</legend>
+                    <div className="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div className="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+                    </div>
+                
+                    <button typw="button" className="btn btn-primary">Sign in</button>
+                    <button type="button" className="btn btn-secondary">Sign up</button>
+            </fieldset>
         </form>
         </div>
         
