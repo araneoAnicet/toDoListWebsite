c8a62b9cbf935b298ee500b7fe7630e18f54c109
DeonOni <maksimtrush905@gmail.com>
2020-06-07 13:44:47 +0300

    add checkbox

--- myfront/src/Components/LoginForm.js
+++ myfront/src/Components/LoginForm.js
@@ -9,23 +9,30 @@ class LoginForm extends Component {
     return(
         
         <div>
-        <form className="LoginForm-margin">
-            <fieldset>
-                <legend>Log in</legend>
-                    <div className="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
-                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                    </div>
-                    <div className="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"></input>
-                    </div>
-                
-                    <button typw="button" className="btn btn-primary">Sign in</button>
-                    <button type="button" className="btn btn-secondary">Sign up</button>
-            </fieldset>
-        </form>
+            <form className="LoginForm-margin">
+                <fieldset>
+                    <legend>Log in</legend>
+                    
+                        <div className="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+                            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+
+                        <div className="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+                        </div>
+
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div>
+                    
+                        <button type="button" className="btn btn-primary">Sign in</button>
+                        <button type="button" className="btn btn-secondary">Sign up</button>
+                </fieldset>
+            </form>
         </div>
         
 )
